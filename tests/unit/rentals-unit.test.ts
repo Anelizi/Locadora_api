93681dc332f81b56a9c8a920401fea39d8e060d4
Anelizi <anelizicarvalho@gmail.com>
2023-07-20 20:00:01 -0300

    test: create rentals

--- tests/unit/rentals-unit.test.ts
+++ tests/unit/rentals-unit.test.ts
@@ -1,9 +1,11 @@
 import rentalsService from "../../src/services/rentals-service";
 import rentalsRepository from "../../src/repositories/rentals-repository";
 import { faker } from "@faker-js/faker";
-import { User } from "@prisma/client";
+import { Rental, User } from "@prisma/client";
 import prisma from "../../src/database";
 import { buildRentalReturn } from "../factories/rental-factory";
+import usersRepository from "repositories/users-repository";
+import { buildUserInput } from "../factories/user-factory";
 
 // para limpar todos os mock
 beforeEach(() => {
@@ -35,4 +37,38 @@ describe("Rentals Service Unit Tests", () => {
     const rental = await rentalsService.getRentals();
     expect(rental).toHaveLength(1);
   });
+
+  describe("create rentals",() => {
+    it("should throw an error when user does not exist",async () => {
+      jest.spyOn(usersRepository, 'getById').mockResolvedValue(null);
+
+      const promise = rentalsService.createRental({
+        userId: 1,
+        moviesId: [1, 2, 3, 4]
+      })
+      expect(promise).rejects.toEqual({
+        name: "NotFoundError",
+        message: "User not found."
+      })
+    })
+
+    it("should throw an error when user already has a rental",async () => {
+      const userMock: User = { id: 1, ...buildUserInput(true)}
+      const userRental: Rental = buildRentalReturn(1);
+
+      jest.spyOn(usersRepository, 'getById').mockRejectedValue(userMock);
+      jest.spyOn(rentalsRepository, 'getRentalById').mockRejectedValue([userRental])
+
+      const promise = rentalsService.createRental({
+        userId: userMock.id,
+        moviesId: [1]
+      });
+
+      expect(promise).rejects.toEqual({
+        name: "PendentRentalError",
+        message: "The user alresdy have a rental!"
+      })
+
+    })
+  })
 });
